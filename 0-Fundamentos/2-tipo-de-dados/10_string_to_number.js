eead57b16d7435a41f3302971a15093d51539c25
Guilherme Aquino Costa <78930198+guilheme-aqlx@users.noreply.github.com>
2025-09-26 14:30:41 +0000

    conversão de tipos

---
+++ 0-Fundamentos/2-tipo-de-dados/10_string_to_number.js
@@ -0,0 +1,12 @@
+
+
+let value1 = "23"
+let value2 = "3.141592"
+
+console.log(value1, Number(value1), typeof Number(value1))
+console.log(value1, Number.parseInt(value1), typeof Number.parseInt(value1))
+
+console.log()
+
+console.log(value2, Number(value2), typeof Number(value2))
+console.log(value2, Number.parseFloat(value2), typeof Number.parseInt(value2))
---
+++ 0-Fundamentos/2-tipo-de-dados/11_number_to_string.js
@@ -0,0 +1,6 @@
+
+
+let value = 29
+
+console.log(typeof value.toString())
+console.log(typeof String(value))
