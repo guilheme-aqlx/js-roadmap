ba1d13b444b97a9efa24b53d382699dcf7d3e96f
Guilherme Aquino Costa <78930198+guilheme-aqlx@users.noreply.github.com>
2025-09-26 18:05:49 +0000

    entendendo a diferença entre Igual e iferente vs Estritamente Igual e Diferente

---
+++ 0-Fundamentos/2-tipo-de-dados/13_operadores.js
@@ -0,0 +1,21 @@
+// Igual e iferente vs Estritamente Igual e Diferente
+
+
+console.log(`${"-".repeat(10)} Igual e Diferente ${"-".repeat(10)}`)
+console.log()
+console.log(`1 == 1: ${1 == 1}`)
+console.log(`1 == "1": ${1 == "1"}`)
+console.log(`1 == true: ${1 == true}`)
+console.log(`1 != true: ${1 == true}`)
+console.log(`0 == false: ${1 == true}`)
+console.log(`0 != false: ${1 == true}`)
+console.log()
+console.log(`${"-".repeat(3)} Estritamente Igual e Diferente ${"-".repeat(3)}`)
+console.log()
+console.log(`1 === 1: ${1 === 1}`)
+console.log(`1 === "1": ${1 === "1"}`)
+console.log(`1 === true: ${1 === true}`)
+console.log(`1 !== true: ${1 !== true}`)
+console.log(`0 === false: ${1 === true}`)
+console.log(`0 !== false: ${1 !== true}`)
+
