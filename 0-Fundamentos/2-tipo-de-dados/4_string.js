5d422dbb710dab54ef298b4a8fc3f6a2ef44b5d8
Guilherme Aquino Costa <78930198+guilheme-aqlx@users.noreply.github.com>
2025-09-26 12:25:03 +0000

    usando apostrofo no JS

---
+++ 0-Fundamentos/2-tipo-de-dados/4_string.js
@@ -0,0 +1,14 @@
+
+let user = 'guilherme-aqlx'
+
+console.log(typeof(user))
+console.log(typeof(user) == 'string')
+
+/*
+Interessante essa forma de usar uma função, sem precisar colocar o
+argumento dentro dos parenteses
+*/ 
+
+console.log(typeof user)
+console.log(typeof user == 'string')
+
---
+++ 0-Fundamentos/2-tipo-de-dados/5_string.js
@@ -0,0 +1,14 @@
+// usando apostrofo para criar quebra de linha
+
+let texto = `"Apóstrofo" (sinal gráfico) é um sinal de pontuação que indica a 
+    supressão de letras numa palavra, como em 
+    "cobra-d'água" (de água) ou "Sant'Ana" (de Ana).
+    `
+
+console.log(`"Apóstrofo" (sinal gráfico) é um sinal de pontuação que indica a 
+    supressão de letras numa palavra, como em 
+    "cobra-d'água" (de água) ou "Sant'Ana" (de Ana).
+    `)
+
+console.log('\n')
+console.log(texto)
