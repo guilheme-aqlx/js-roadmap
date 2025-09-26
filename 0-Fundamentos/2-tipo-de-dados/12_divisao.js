440b3f88cfc0ea0b4523890f41ce4796089a4ca0
Guilherme Aquino Costa <78930198+guilheme-aqlx@users.noreply.github.com>
2025-09-26 14:58:32 +0000

    operador de divisão

---
+++ 0-Fundamentos/2-tipo-de-dados/12_divisao.js
@@ -0,0 +1,12 @@
+
+
+// Divisão de um numero
+console.log(31 / 2)
+
+// Resto da divisão
+console.log(31 % 2)
+
+// Divisão inteira
+console.log(31 / 2 | 0)
+console.log(Math.floor(31 / 2))
+console.log(Math.trunc(31 / 2))
