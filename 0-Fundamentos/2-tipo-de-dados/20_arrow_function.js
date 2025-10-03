baf697a15505c9658d3a77a8632c80490b8c0bc7
Guilherme Aquino Costa <78930198+guilheme-aqlx@users.noreply.github.com>
2025-10-03 01:57:14 +0000

    usando arrow function

---
+++ 0-Fundamentos/2-tipo-de-dados/20_arrow_function.js
@@ -0,0 +1,17 @@
+
+
+
+// Função normal
+function potencia1 (base, expoente){
+    return base ** expoente
+}
+
+console.log(potencia1(2, 8))
+
+// Arrow Function
+const potencia2 = (base, expoente) => {
+    return base ** expoente
+}
+
+console.log(potencia2(2, 10))
+
